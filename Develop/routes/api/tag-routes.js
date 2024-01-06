0dc38503d5afe31c15d277fae0c4be80f563641a
Ida Whitcomb <idawhitcomb@gmail.com>
2024-01-06 14:37:08 -0500

    Added routes for Tag category

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,94 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try{
+    const tags = await Tag.findAll({
+      include:{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+        through: {attributes: [] }
+      }
+    })
+    res.json(tags);
+  } catch (err) {
+  console.error(err);
+  res.status(500).json({ error: 'Internal Server Error'});
+  }
 });
 
-router.get('/:id', (req, res) => {
+
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try{
+    const tag = await Tag.findByPk({
+      where: {
+        id: req.params.id
+      },
+      include: {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+        through: {attributes: []}
+      }
+    });
+    if (tag) {
+      res.json(tag);
+    } else {
+      res.status(404).json({ error: 'Category not found' });
+    }
+  } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Internal Server Error'});
+  }
 });
 
-router.post('/', (req, res) => {
+
+router.post('/', async (req, res) => {
   // create a new tag
+  try{
+    const tag = await Tag.create({
+      id: req.body.id,
+      tag_name: req.body.tag_name
+    });
+    res.status(201).json(tag);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  };
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  const tag_name = req.body
+  try{
+    const tagToUpdate = Tag.findByPk(req.params.id);
+    if(!tagToUpdate) {
+      return res.status(404).json({ error: 'Tag not found' });
+    };
+    await tagToUpdate.update({ tag_name });
+    res.json(tagToUpdate);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try{
+    const tagToDelete = await Tag.findByPk(req.params.id);
+    if(!tagToDelete) {
+      return res.status(404).json({ error: 'Tag not found' });
+    }
+    await tagToDelete.destroy();
+    res.json({ message: 'Tag deleted successfully '});
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 module.exports = router;
